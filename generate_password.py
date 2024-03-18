da46d05d44a2917c3009a20cf49ccfdf1dc6ebae
Hai Guan <davenkwek2010@gmail.com>
2024-03-18 20:25:01 +0800

    Back-end so far

--- generate_password.py
+++ generate_password.py
@@ -1,5 +1,7 @@
+#Imports
 import random
 import string 
+from tkinter import messagebox
 pun = string.punctuation
 letters = string.ascii_letters
 numbers = string.digits
@@ -12,3 +14,17 @@ def generate_password(len:int):
     
 password1 = generate_password(5)
 print(password1)
+
+def gui_generate_password(website, login_id, include_uppercase, include_lowercase, include_special_characters, include_numbers, password_length):
+    characters = ""
+    if include_uppercase:
+        characters += string.ascii_uppercase
+    if include_lowercase:
+        characters += string.ascii_lowercase
+    if include_special_characters:
+        characters += string.punctuation
+    if include_numbers:
+        characters += string.digits
+    if not characters:
+        messagebox.showerror("Error", "Please select at least one character type.")
+        return
